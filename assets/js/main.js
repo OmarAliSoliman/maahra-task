32b5786ed1f4647360648af23c98a4bf85b8086d
OmarAliSoliman <omarasmar9955@gmail.com>
2021-08-06 04:05:37 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+  // choses-slider
+
+  // AOS.init();
+
+  if ($(".bg-sidenavOpen").length) {
+    $(".bg-sidenavOpen").on("click", function () {
+      $("#mySidenav").css("right", "-400px");
+      $(this).css("display", "none");
+      document.body.classList.remove("openMenuActive");
+    });
+  }
+
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 20) {
+      $(".mynavbar").addClass("fixed-navbar");
+      $("#floating").css("opacity", "1");
+    } else {
+      $(".mynavbar").removeClass("fixed-navbar");
+      $("#floating").css("opacity", "0");
+    }
+  });
+
+  $("#floating").click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      50
+    );
+  });
+
+  var swiper = new Swiper(".swiper-container", {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    speed: 500,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    on: {
+      init: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("animate");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionStart: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionEnd: function () {
+        $(".swiper-progress-bar").eq(0).addClass("animate");
+      },
+    },
+  });
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.right = "0";
+  document.querySelector(".bg-sidenavOpen").style.display = "block";
+  document.body.classList.add("openMenuActive");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.right = "-400px";
+  document.querySelector(".bg-sidenavOpen").style.display = "none";
+  document.body.classList.remove("openMenuActive");
+}
